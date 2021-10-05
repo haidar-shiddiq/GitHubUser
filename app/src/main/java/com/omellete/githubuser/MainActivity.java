c51f6ba9239f7843e04d975aa0e071c11f03072c
haidar-shiddiq <hdrshiddiq@gmail.com>
2021-10-05 14:21:22 +0700

    searchview

--- app/src/main/java/com/omellete/githubuser/MainActivity.java
+++ app/src/main/java/com/omellete/githubuser/MainActivity.java
@@ -1,20 +1,17 @@
 package com.omellete.githubuser;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.app.ProgressDialog;
-import android.widget.TextView;
-import android.view.KeyEvent;
+import android.app.SearchManager;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -22,10 +19,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     MyViewModel searchViewModel;
-    ImageView clear;
     ProgressDialog loadingMessage;
     RecyclerView rv_main;
-    EditText searchfield;
     UserListAdapter userListAdapter;
 
     @Override
@@ -33,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        searchfield = findViewById(R.id.fieldSearch);
         rv_main = findViewById(R.id.rv_main);
         loadingMessage = new ProgressDialog(this);
         loadingMessage.setCancelable(false);
@@ -43,34 +37,6 @@ public class MainActivity extends AppCompatActivity {
         rv_main.setAdapter(userListAdapter);
         rv_main.setHasFixedSize(true);
 
-        clear = findViewById(R.id.clear);
-        clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                searchfield.getText().clear();
-//                rv_main.setVisibility(View.GONE);
-            }
-        });
-
-        searchfield.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                String strUsername = searchfield.getText().toString();
-                if (strUsername.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please type a userneme", Toast.LENGTH_SHORT).show();
-                } else {
-                    if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                        loadingMessage.show();
-                        searchViewModel.searchUsername(strUsername);
-                        InputMethodManager inputMethodManager = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                        inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                        clear.setVisibility(View.VISIBLE);
-                        return true;
-                    }
-                }
-                return false;
-            }
-        });
 
         searchViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(MyViewModel.class);
         searchViewModel.getResultList().observe(this, new Observer<ArrayList<SearchModel>>() {
@@ -84,6 +50,38 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        if (searchManager != null) {
+            SearchView searchView = (SearchView) (menu.findItem(R.id.search)).getActionView();
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setQueryHint(getResources().getString(R.string.search_hint));
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    if (query.isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Please type a userneme", Toast.LENGTH_SHORT).show();
+                    } else {
+                            loadingMessage.show();
+                            searchViewModel.searchUsername(query);
+                            return true;
+                    }
+                    return false;
+                }
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+        }
+        return true;
     }
 
 }
